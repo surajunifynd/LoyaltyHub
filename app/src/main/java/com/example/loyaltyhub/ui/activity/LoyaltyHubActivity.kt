cd5064cb7afdd7297e302d7d4daf06694fa57195
suraj <suraj@unifynd.com>
2023-04-27 18:31:25 +0530

    code changes

--- app/src/main/java/com/example/loyaltyhub/ui/activity/LoyaltyHubActivity.kt
+++ app/src/main/java/com/example/loyaltyhub/ui/activity/LoyaltyHubActivity.kt
@@ -3,6 +3,7 @@ package com.example.loyaltyhub.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.bumptech.glide.Glide
 import com.example.loyaltyhub.R
 import com.example.loyaltyhub.databinding.ActivityLoyaltyhubBinding
 import com.example.loyaltyhub.model.ServerResponseData
@@ -34,6 +35,7 @@ class LoyaltyHubActivity : AppCompatActivity(), ProfileView, LoyaltyView,
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik5hUm1JVGJTW3Q0U2lucENVRVhUcVEkJCIsImZpcnN0TmFtZSI6IlN1cmFqIiwibGFzdE5hbWUiOiJTYWhhbmkiLCJlbWFpbCI6InN1cmFqQHVuaWZ5bmQuY29tIiwibW9iaWxlTnVtYmVyIjo3ODI3MDcwNTM5LCJkZXZpY2UiOiIyIiwiZGV2aWNlSWQiOiJiNTg4ZGZhNC00MTcwLTQxODMtOWIxNS1iYTM2MGQ3NWQ0YjQiLCJidWlsZCI6IjYuMi4xLjUiLCJwcmVmZXJyZWRNYWxsSWQiOm51bGwsImlzRXhjbHVzaXZlIjowLCJ3aGF0c0FwcENvbmNlbnQiOiIxIiwib3B0ZWRGb3JEZWxldGlvbiI6MCwicmVhY3RpdmF0aW9uQ29kZSI6bnVsbCwicHJvZmlsZVBpYyI6Imh0dHBzOi8vc3RhZ2UtYXNnYXJkLXByaXZhdGUuczMuYXAtc291dGgtMS5hbWF6b25hd3MuY29tL3Byb2ZpbGVQaWMvMjAyMi8xMC8yMC8xODIxMzg2MzkzNy00NTA1NjAtMjAyMi0xMC0yMC5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBNVlMNlRVTUVUNEdCUEVHRSUyRjIwMjMwNDI0JTJGYXAtc291dGgtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyMzA0MjRUMDc1MzQ1WiZYLUFtei1FeHBpcmVzPTM2MDAmWC1BbXotU2lnbmF0dXJlPTAzMGJjNmNhMDNkODczZWFhM2Q5NmRlZmRjOTYxMzU5YjQxMTg4OTgwMWMxNmFjNDkwMTkwZTExMDIwMjk1NzImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0IiwibGFzdExvZ2luIjoiMjQtMDQtMjAyMyIsImRlbGV0aW9uUmVxdWVzdGVkQXQiOm51bGwsImlhdCI6MTY4MjMyMjgyNSwiZXhwIjoxNjgzNjE4ODI1fQ.Z3qqUr__aQLS2wQwB9ru1CUiCIoyD5na_udeGpPbkg4"
         profilePresenter = ProfilePresenter(this)
         loyaltyHubPresenter = LoyaltyHubPresenter(this)
+        binding.ivProfile.let { Glide.with(this).load(serverResponseData?.userData?.profilePic).into(it) }
     }
 
     override fun onResume() {
@@ -74,8 +76,9 @@ class LoyaltyHubActivity : AppCompatActivity(), ProfileView, LoyaltyView,
 
     override fun onServerProfileDetailsReceived(user: User) {
         this.user = user
-        //binding.tvPointsMain.text = user.totalPoints.toString()
+        binding.tvPointsMain.text = user.totalPoints.toString()
         binding.tvGemsMain.text = user.gemBalance.toString()
+        binding.tvBadgePoint.text = user.earnedPoints.toString()
     }
 
     override fun onServerLoyaltyHubReceived(loyaltyHub: ServerResponseData) {
